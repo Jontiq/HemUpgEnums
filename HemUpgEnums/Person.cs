616fb5248a42e2c835025715b610da8a3bf9aabb
Jontiq <99jonathancar@gmail.com>
2025-10-01 23:18:03 +0200

    Add project files.

---
+++ HemUpgEnums/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HemUpgEnums
+{
+    internal class Person
+    {
+        //Props
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        private int _age;
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Invalid age, age can't be less than 1.");
+                    _age = 1;
+                }
+                else
+                {
+                    _age = value;
+                }
+            }
+        }
+
+        //Construktor
+        public Person(string FirstName, string LastName, int Age)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Age = Age;
+        }
+
+        //Methods
+        public string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
---
+++ HemUpgEnums/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HemUpgEnums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Adam", "Larsson", 34);
+
+            string fullNamePerson1 = person1.GetFullName();
+            Console.WriteLine(fullNamePerson1);
+        }
+    }
+}
